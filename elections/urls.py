c1d7e5dbbe711a5a2754914cc273f6561a02f381
onajifortune <onajifortunejnr@gmail.com>
2022-09-25 09:33:42 +0100

    new

--- elections/urls.py
+++ elections/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('polling_units', views.list_polling_units, name='polling_units'),
+    path('', views.list_polling_units, name='polling_units'),
     path('polling_units/<int:uniqueid>', views.retrieve_polling_unit, name='polling_unit'),
     path('local_govt', views.local_government, name='local_govt'),
     path('add_results', views.result_for_new_polling_unit, name='add_results'),
