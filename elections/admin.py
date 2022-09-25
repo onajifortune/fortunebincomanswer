f3f0d29cac652b778282632c59c41f0c8dec8125
onajifortune <onajifortunejnr@gmail.com>
2022-09-25 09:54:23 +0100

    new

--- elections/admin.py
+++ elections/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import AnnouncedPuResults, Lga, Party, PollingUnit
+
 # Register your models here.
+
+admin.site.register(AnnouncedPuResults)
+admin.site.register(PollingUnit)
+admin.site.register(Party)
+admin.site.register(Lga)
