922504dd1fa610ce526a2c73049710786946fc44
onajifortune <onajifortunejnr@gmail.com>
2022-10-08 08:47:15 +0100

    New

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bincomtestdb',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': os.environ.get('DB_PASSWORD')
